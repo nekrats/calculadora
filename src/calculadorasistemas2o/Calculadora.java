a2caf3d3c2dda8d578a0b90ca459990ee9b1b8e0
stark <stark@DESKTOP-KGMMH3C>
2018-09-26 16:20:28 +0200

    Pedir 2 numeros + Scanner

--- src/calculadorasistemas2o/Calculadora.java
+++ src/calculadorasistemas2o/Calculadora.java
@@ -1,8 +1,15 @@
 package calculadorasistemas2o;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
     public static void main(String[] args) {
-
+        int a, b;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Pasame 1 numeros");
+        a = in.nextInt();
+        System.out.println("Pasame un 2o numero");
+        b = in.nextInt();
     }
 }
