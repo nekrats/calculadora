b3644787f32e88e56a8b23fdf9170081bd9a398a
stark <stark@DESKTOP-KGMMH3C>
2018-09-26 16:40:55 +0200

    Funcion suma

--- src/calculadorasistemas2o/Calculadora.java
+++ src/calculadorasistemas2o/Calculadora.java
@@ -3,13 +3,22 @@ package calculadorasistemas2o;
 import java.util.Scanner;
 
 public class Calculadora {
-
+ 
     public static void main(String[] args) {
-        int a, b;
         Scanner in = new Scanner(System.in);
+        int a, b;
+        int x,y;
         System.out.println("Pasame 1 numeros");
         a = in.nextInt();
         System.out.println("Pasame un 2o numero");
         b = in.nextInt();
+        suma(x);
+    }
+    private static int suma (int x){
+        int a,b,suma;
+        a=in.nextInt();
+        b=in.nextInt();
+        suma = a+b;
+        return x;
     }
 }
